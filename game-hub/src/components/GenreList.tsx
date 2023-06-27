6ada7790c20e11f74666d9a53b284ce9fe28f396
devdhark <devdhar.kodiatar@gmail.com>
2023-06-26 23:12:54 -0500

    add commas dynamically

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -14,8 +14,11 @@ interface Props {
 const GenreList = ({ genres }: Props) => {
   return (
     <HStack marginY={1}>
-      {genres.map((genre) => (
-        <Text key={genre.id}>{genre.name}</Text>
+      {genres.map((genre, index) => (
+        <Text key={genre.id}>
+          {genre.name}
+          {index != genres.length - 1 && ","}
+        </Text>
       ))}
     </HStack>
   );
