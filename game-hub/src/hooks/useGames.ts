9fef16b72414e75b744eef3e4417913403d39d46
devdhark <devdhar.kodiatar@gmail.com>
2023-06-27 19:00:49 -0500

    create generic hooks

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -1,8 +1,6 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
 import { Platform } from "../components/PlatformIconList";
 import { Genre } from "./useGenres";
+import useData from "./useData";
 
 export interface Game {
   id: number;
@@ -13,38 +11,6 @@ export interface Game {
   genres: Genre[]
 }
 
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
-
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false)
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-
-    apiClient
-      .get<FetchGamesResponse>("/games", {
-        signal: controller.signal,
-      })
-      .then((res) => {
-        setGames(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading };
-};
+const useGames = () => useData<Game>('/games')
 
 export default useGames;
